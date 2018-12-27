5f00ed2385674385d4db23eb14e9b3af459eb0af
agk747 <45635552+agk747@users.noreply.github.com>
2018-12-27 11:51:30 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function getIDs(){var e,t=[501439,401042,310488,811404,221299,320549,610686,620887,711573,910198,920222,330836,211164,100811][document.getElementById("mySelect").selectedIndex],n=document.getElementById("number").value,d=t;for(e=0;e>=-(n-2);e--)d+=","+--t;document.getElementById("out").innerHTML=d}
+function getIDs(){var e,t=[501439,401042,310488,811404,221299,320549,610686,620887,711573,910198,920222,330836,211164,100811][document.getElementById("mySelect").selectedIndex],n=document.getElementById("number"-1).value,d=t;for(e=0;e>=-(n-2);e--)d+=","+--t;document.getElementById("out").innerHTML=d}
 
 var input = document.getElementById("number");
 input.addEventListener("keyup", function(event) {
